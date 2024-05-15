bb7076b482982bc8ab997805537eb1ae0b4a4d4d
Andrei Ukladchikov <andrei.ukladchikov@auriga.com>
2024-05-15 20:03:52 +0300

    Add project files.

---
+++ Miner/Miner.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace MinerApp
+{
+    public class Miner
+    {
+        private readonly ILogger _logger;     
+        private int _totalCoins = 0;
+
+        public Miner(ILogger logger)
+        {
+            _logger = logger;               
+        }
+
+        private void AddCoins()
+        {
+            Random rnd = new Random();           
+            int coinsToAdd = rnd.Next(10, 50);
+            _totalCoins += coinsToAdd;
+
+            _logger.LogInformation("Added {} coins.", coinsToAdd);
+            _logger.LogInformation("Total amount is {} coins.", _totalCoins);
+        }
+
+        public void Mine()
+        {
+            Random rnd = new Random();
+            bool doWork = true;
+            while (doWork)
+            {
+                var timer = 1000 * rnd.Next(1, 10);
+                Thread.Sleep(timer);
+
+                AddCoins();
+            }
+        }
+    }
+}
---
+++ Miner/Program.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.Logging;
+using MinerApp;
+
+using ILoggerFactory factory = LoggerFactory.Create(builder =>
+{
+    builder.AddConsole();
+});
+ILogger logger = factory.CreateLogger("Program");
+
+
+Miner miner = new Miner(logger);
+miner.Mine();
